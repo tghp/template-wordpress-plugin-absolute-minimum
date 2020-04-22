dee74affb023394f7ba781788fb85c0fd15bab74
Josh Davenport <josh.davenport@tghp.co.uk>
2020-04-22 09:58:08 +0100

    Initial commit

---
+++ plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Plugin
+ * Version:     1.0.0
+ * Author:      TGHP
+ *
+ */
+
+/*
+1. Rename me and folder to name of plugin.
+2. Change name in header above
+3. Change name in composer.json
+4. Develop!
+*/
